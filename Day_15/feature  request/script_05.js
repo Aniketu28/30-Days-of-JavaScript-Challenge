4a03387d7443877e73515651f4238df948194a7b
Aniket uagre <ugareaniket28@gmail.com>
2024-08-01 00:43:43 +0530

    day 15completed

---
+++ Day_15/feature  request/script_05.js
@@ -0,0 +1,15 @@
+function numToFact(num){
+    let fact = 1
+    function calculateFact(){
+        for(let i=1; i<=num; i++){
+             fact *= i;
+        }
+    }
+    
+    calculateFact();
+
+    return fact;
+}
+
+let fact = numToFact(5)
+console.log(fact);
